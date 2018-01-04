7258003c6c2885ebe84edd909b68f96a6ee35834
Pedro Durek <pedro.durek@kantaribopemedia.com>
2018-01-04 12:54:39 -0200

    feat(Filter): Filter to sort the posts included

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react'
 import * as PostsAPI from '../api/PostsAPI'
-import ListPosts from './ListPosts'
 import { Route } from 'react-router-dom'
+import sortBy from 'sort-by'
+import FaSortNumericAsc from 'react-icons/lib/fa/sort-numeric-asc'
+import FaSortNumericDesc from 'react-icons/lib/fa/sort-numeric-desc'
+import ListPosts from './ListPosts'
+
 
 class App extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        filterOptions: [{
+            key: 'voteScore',
+            icon: () => (<FaSortNumericAsc size={20} />),
+            text: 'Score'
+        }, {
+            key: '-voteScore',
+            icon: () => (<FaSortNumericDesc size={20} />),
+            text: 'Score'
+        }, {
+            key: 'timestamp',
+            icon: () => (<FaSortNumericAsc size={20} />),
+            text: 'Date'
+        }, {
+            key: '-timestamp',
+            icon: () => (<FaSortNumericDesc size={20} />),
+            text: 'Date'
+        }]
     }
 
     componentDidMount() {
 
         PostsAPI.getAll().then((posts) => {
-            this.setState({ posts })
+            console.log(posts)
+            this.setState({ posts: posts.sort(sortBy('-voteScore')) })
         })
 
     }
@@ -22,18 +44,33 @@ class App extends Component {
         PostsAPI.voting(postId, vote).then((p) => {
             this.setState(({ posts }) => ({
                 posts: posts.filter((post) => post.id !== p.id).concat(p)
+                    .sort(sortBy('-voteScore'))
             }))
         })
 
     }
 
+    filterHandler = (sort) => {
+
+        console.log(sort)
+        this.setState(({ posts }) => ({
+            posts: posts.sort(sortBy(sort))
+        }))
+
+    }
+
     render() {
         
-        const { posts } = this.state
+        const { posts, filterOptions } = this.state
         return (
             <div className="app">
                 <Route exact path="/" render={() => (
-                    <ListPosts posts={posts} updateVote={this.updateVote} />
+                    <ListPosts 
+                        posts={posts} 
+                        filterOptions={filterOptions}
+                        filterHandler={this.filterHandler}
+                        updateVote={this.updateVote} 
+                    />
                 )}/>
             </div>
         )
