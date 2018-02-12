f7ae31034b5cd96adf50301d233be1567760d6f5
Pedro Henrique Marinho Durek <pedro_hmd@hotmail.com>
2018-02-12 19:27:37 -0200

    refact: indent adjustments

--- frontend/src/actions/categories.js
+++ frontend/src/actions/categories.js
@@ -4,21 +4,21 @@ export const REQUEST_CATEGORIES = 'REQUEST_CATEGORIES'
 export const GET_ALL_CATEGORIES = 'GET_ALL_CATEGORIES'
 
 export const fetchAllCategories = (callback = () => {}) => (dispatch) => {
-	dispatch(setIsFetching(true))
-	CategoriesAPI.getAll().then((data) => {
-		dispatch(getAllCategories(data))
-		const { categories } = data
-		callback((categories.length > 0)?categories[0].name:'')
-	})
+    dispatch(setIsFetching(true))
+    CategoriesAPI.getAll().then((data) => {
+        dispatch(getAllCategories(data))
+        const { categories } = data
+        callback(categories.length > 0 ? categories[0].name : '')
+    })
 }
 
 const setIsFetching = (isFetching) => ({
-	type: REQUEST_CATEGORIES,
-	isFetching
+    type: REQUEST_CATEGORIES,
+    isFetching
 })
 
 const getAllCategories = ({ categories }) => ({
-	type: GET_ALL_CATEGORIES,
-	categories,
-	isFetching: false
+    type: GET_ALL_CATEGORIES,
+    categories,
+    isFetching: false
 })
