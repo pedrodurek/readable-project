a2a0436593204bd5752f87cab3e06e5cc4fb20ae
Pedro Durek <pedro_hmd@hotmail.com>
2018-02-15 12:25:01 -0200

    feat: Page not found included

---
+++ frontend/src/views/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import '../styles/NotFound.css'
+
+const NotFound = () => (
+    <div class="not-found">
+        <h1>404</h1>
+        <h3>Page not found</h3>
+    </div>
+)
+
+export default NotFound
